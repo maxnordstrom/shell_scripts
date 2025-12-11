bf7fa29352d143232590ad7a38c6f8c660005e52
Max <26596537+maxnordstrom@users.noreply.github.com>
2025-12-11 11:23:48 +0100

    adding comments

--- Bash/basic-security-check.sh
+++ Bash/basic-security-check.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Skapa variabel
 logfile="security_log.txt"
 
+# Deklarera funktionen log
+# Echo skriver ut datumet (en inbyggd method?) och $1 innehåller värdet 
+# från antingen $file_input eller $user_input beroende på i vilken funktion
+# log används.
+# Den vänstra halvan pipe:as till tee
+# tee skapar en fil om den inte finns, annars lägger den till värdet till $logfile
+# $logfile är som bekant security_log.txt
 log() {
   echo "$(date) - $1" | tee -a $logfile
 }
